77bdbdea29db7443a3e9f3b8fc175a9b59d97a0a
hochenchong <hochenchong@163.com>
2018-10-10 22:20:21 +0800

    饿汉式单例模式类编写

--- src/main/java/pattern/singleton/SingletonDemo01.java
+++ src/main/java/pattern/singleton/SingletonDemo01.java
@@ -1,8 +1,21 @@
 package pattern.singleton;
 
+/**
+ * @Description: 饿汉式单例模式
+ * @author: HochenChong
+ * @date: 2018-10-10
+ * @version v0.1
+ */
 public class SingletonDemo01 {
+	// 类初始化时，立即加载此对象
+	private static SingletonDemo01 instance = new SingletonDemo01();
+	
 	// 构造方法私有化
 	private SingletonDemo01() {
-		
 	}
+
+	public static SingletonDemo01 getInstance() {
+		return instance;
+	}
+	
 }
