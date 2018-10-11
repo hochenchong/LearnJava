4c7f64128e12eaa072f3275a4e2899f62cb925d3
hochenchong <hochenchong@163.com>
2018-10-11 22:14:40 +0800

    使用反射创建饿汉式单例模式类创建多个对象

--- src/test/java/pattern/singleton/SingletonDemo01Test.java
+++ src/test/java/pattern/singleton/SingletonDemo01Test.java
@@ -1,6 +1,9 @@
 package pattern.singleton;
 
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Constructor;
+
 import org.junit.Test;
 
 /**
@@ -26,5 +29,22 @@ public class SingletonDemo01Test {
 		
 		// s1 == s2 是否为 true，即 s1 与 s2 是否为同一个对象
 		assertSame(s1, s2);
+	}
+	
+	/**
+	 * 使用反射创建多个对象
+	 * @throws Exception 这里简单处理，直接将异常抛出
+	 */
+	@Test
+	public void testReflectBug() throws Exception {
+		SingletonDemo01 s1 = SingletonDemo01.getInstance();
+		
+		Class<SingletonDemo01> clz = (Class<SingletonDemo01>) s1.getClass();
+		Constructor<SingletonDemo01> constructor = clz.getDeclaredConstructor(null);
+		// 设置 accessible 为 true，跳过权限检查
+		constructor.setAccessible(true);
+		SingletonDemo01 s2 = constructor.newInstance();
+
+		assertNotEquals(s1, s2);
 	}
 }
